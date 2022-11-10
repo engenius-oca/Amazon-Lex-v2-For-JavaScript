3c4b97365cb7151eeee0856520dfed4aa7288896
userkitashin <2004230005kr@edu.oca.ac.jp>
2022-11-10 11:15:02 +0000

    update lex.js

--- static/lex.js
+++ static/lex.js
@@ -87,7 +87,7 @@ function showResponse(lexResponse) {
         var responsePara = document.createElement("P");
         responsePara.className = 'lexResponse';
         if (lexResponse.messages) {
-                responsePara.appendChild(responsePara.appendChild(lexResponse.messages[0]['content']));
+            (responsePara.appendChild(lexResponse.messages[0]['content']);
                 responsePara.appendChild(document.createElement('br'));
         }
         if (lexResponse.dialogState === 'ReadyForFulfillment') {
